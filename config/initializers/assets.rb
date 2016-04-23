32de23fb3a74ddfe9fcf0810fa3af19d9f97209d
vodkaiserz <vodkaiserz@gmail.com>
2016-04-24 00:47:08 +0700

    adding font-awesome

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join("vendor/bootstrap-3.3.6-dist")
+Rails.application.config.assets.paths << Rails.root.join("vendor/font-awesome-4.6.1")
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
